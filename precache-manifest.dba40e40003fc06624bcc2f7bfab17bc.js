d7ef4aef67b34ee1e4badcecae9548822ca7481f
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2019-08-20 19:23:05 -0300

    Updates

---
+++ precache-manifest.dba40e40003fc06624bcc2f7bfab17bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51919173ebc580b3ee4edffa54560d5f",
+    "url": "/lguibr/profile/index.html"
+  },
+  {
+    "revision": "e8257e9cf9a6d65d74dd",
+    "url": "/lguibr/profile/static/css/main.1f7df258.chunk.css"
+  },
+  {
+    "revision": "ef28f14e217a343a0e9f",
+    "url": "/lguibr/profile/static/js/2.979431a4.chunk.js"
+  },
+  {
+    "revision": "e8257e9cf9a6d65d74dd",
+    "url": "/lguibr/profile/static/js/main.67d8de78.chunk.js"
+  },
+  {
+    "revision": "1de8031dcb261e1d99c0",
+    "url": "/lguibr/profile/static/js/runtime~main.61566ed2.js"
+  }
+]);
